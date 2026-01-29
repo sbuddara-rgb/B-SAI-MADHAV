d2965ff22e1be4760aa19216220bc08cedb98887
Buddaraju Sai Madhav <sbuddara@student.gitam.edu>
2026-01-29 15:25:33 +0530

    Update ACKERMANN.C

--- ACKERMANN.C
+++ ACKERMANN.C
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-int ackermann(int m, int n);
-
-int main() {
-    int m, n;
-
-    scanf("%d %d", &m, &n);
-
-    printf("Ackermann(%d, %d) = %d", m, n, ackermann(m, n));
-    return 0;
+unsigned long long ackermann(unsigned int m, unsigned int n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (m > 0 && n == 0) {
+        return ackermann(m - 1, 1);
+    } else {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
 }
 
-int ackermann(int m, int n) {
-    if (m == 0)
-        return n + 1;
-    else if (m > 0 && n == 0)
-        return ackermann(m - 1, 1);
-    else
-        return ackermann(m - 1, ackermann(m, n - 1));
+int main() {
+    unsigned int m, n;
+    
+    printf("Enter non-negative integers m and n: ");
+    scanf("%u %u", &m, &n);
+    
+    printf("Ackermann(%u, %u) = %llu\n", m, n, ackermann(m, n));
+    
+    return 0;
 }
