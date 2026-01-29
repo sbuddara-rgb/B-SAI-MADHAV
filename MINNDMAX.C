399b3c6152f236913b83ebe6eafea391df878932
Buddaraju Sai Madhav <sbuddara@student.gitam.edu>
2026-01-29 14:56:32 +0530

    Create MINNDMAX.C

---
+++ MINNDMAX.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i;
+    int max, min;
+
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    max = min = a[0];
+
+    for (i = 1; i < n; i++) {
+        if (a[i] > max)
+            max = a[i];
+        if (a[i] < min)
+            min = a[i];
+    }
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d", min);
+
+    return 0;
+}
