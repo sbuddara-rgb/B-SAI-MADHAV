55bd5bd231750d9f4cea57428e2cc21cf24b86db
Buddaraju Sai Madhav <sbuddara@student.gitam.edu>
2026-01-29 15:11:18 +0530

    Update QUICKSORT.C

--- QUICKSORT.C
+++ QUICKSORT.C
@@ -1,23 +1,45 @@
 #include <stdio.h>
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-void quickSort(int a[], int low, int high);
-int partition(int a[], int low, int high);
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; 
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;  
+}
+
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int p = partition(arr, low, high);
+
+        quickSort(arr, low, p - 1);  
+        quickSort(arr, p + 1, high);  
+    }
+}
 
 int main() {
-    int a[100], n, i;
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
-        scanf("%d", &a[i]);
+    quickSort(arr, 0, n - 1);
 
-    quickSort(a, 0, n - 1);
-
-    for (i = 0; i < n; i++)
-        printf("%d ", a[i]);
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
 
     return 0;
 }
-
-void quickSort(int a[], int low, int high) {
-    if (low < high) {
-        int pi = partition
