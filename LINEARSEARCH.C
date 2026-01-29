4460d6785685203bff5f3b99792c131285da9ec4
Buddaraju Sai Madhav <sbuddara@student.gitam.edu>
2026-01-29 15:21:35 +0530

    Update LINEARSEARCH.C

--- LINEARSEARCH.C
+++ LINEARSEARCH.C
@@ -1,21 +1,16 @@
 #include <stdio.h>
-
-int main() {
-    int a[100], n, key, i;
-
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
-        scanf("%d", &a[i]);
-
-    scanf("%d", &key);
-
-    for (i = 0; i < n; i++) {
-        if (a[i] == key) {
-            printf("Element found at position %d", i + 1);
-            return 0;
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;
         }
     }
-
-    printf("Element not found");
-    return 0;
+    return -1; 
 }
+
+int main() {
+    int arr[] = {5, 3, 7, 1, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+
+    int result = linearSearch(arr, n, tar
