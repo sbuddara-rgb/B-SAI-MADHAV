27c6aa4ee3a64cee8e0a82d9a046a0d30b0dd322
Buddaraju Sai Madhav <sbuddara@student.gitam.edu>
2026-01-29 14:54:59 +0530

    Create EUCLIDALOGORITHM.C

---
+++ EUCLIDALOGORITHM.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int gcd(int a, int b);
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("GCD = %d", gcd(a, b));
+    return 0;
+}
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
